1d6268dd061046c6e90e98f187ae488305cef1a5
Marco Abundo <shrtlist@gmail.com>
2024-08-14 08:43:02 -0700

    Unit tests

--- FetchMealsTests/FetchMealsTests.swift
+++ FetchMealsTests/FetchMealsTests.swift
@@ -10,27 +10,25 @@ import XCTest
 
 final class FetchMealsTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    @MainActor func testfetchDesserts() async {
+        let sut = MealViewModel()
+
+        await sut.loadDesserts()
+
+        XCTAssertTrue(sut.meals.count > 0)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    @MainActor func testfetchMealDetail() async {
+        let idMeal = "52966"
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+        let sut = MealViewModel()
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+        await sut.loadMealDetail(id: idMeal)
 
+        XCTAssertEqual(sut.selectedMeal?.idMeal, idMeal)
+        XCTAssertNotNil(sut.selectedMeal?.strMeal)
+        XCTAssertNotNil(sut.selectedMeal?.strInstructions)
+        XCTAssertNotNil(sut.selectedMeal?.strMealThumb)
+        XCTAssertNotNil(sut.selectedMeal?.ingredientsWithMeasurements)
+    }
 }
